1a6e36afe0f94e5b682754bc82cb9b8f60acca87
nishattasnim <nishattasnim3646@gmail.com>
2026-03-29 15:53:28 +0600

    restart

--- controller/userController.js
+++ controller/userController.js
@@ -81,31 +81,3 @@ export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
     });
 });   
 
-export const getAllDoctors = catchAsyncErrors(async(req, res, next)=>{
-    const doctors = await User.find({ role: "Doctor" });
-    res.status(200).json({
-        success: true,
-        doctors,
-    });
-});
-
-export const getUserDetails = catchAsyncErrors(async(req, res, next)=>{
-    const user = req.user;
-    res.status(200).json({
-        success: true,
-        user,
-    });
-}); 
-
-export const logoutAdmin = catchAsyncErrors(async (req, res, next) => {
-  res
-    .status(201)
-    .cookie("adminToken", "", {
-      httpOnly: true,
-      expires: new Date(Date.now()),
-    })
-    .json({
-      success: true,
-      message: "Admin Logged Out Successfully.",
-    });
-});
